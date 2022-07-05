22ac17015eeaf6db9cc312bf45d641333ef6144c
Kim Younggeon <mail@kyg.kr>
2022-07-05 22:17:38 +0900

    test: contextLoads 테스트 추가

---
+++ src/test/kotlin/packagename/MainTest.kt
@@ -0,0 +1,11 @@
+package packagename
+
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+@SpringBootTest
+internal class Main {
+
+  @Test
+  fun contextLoads() {}
+}
